16a2c85b90a70479b07fd95b2b58e928d16f813e
Jaanuszek <kubex564@gmail.com>
2026-08-16 21:04:20 +0200

    rendered sample gaussian!

--- CPU_gaussian_renderer/include/gaussian.hpp
+++ CPU_gaussian_renderer/include/gaussian.hpp
@@ -1,13 +1,25 @@
 #pragma once
 
 #include "ppm.hpp"
+#include "glm/glm.hpp"
 
-struct gaussian
+#include <vector>
+#include <array>
+
+struct Gaussian
 {
-
+    // Covariance matrix should not be fixed
+    // it supposed to be computed on the fly (answer is in the paper)
+    // but for the simplicity, I will not compute it there XD
+    glm::mat3 cov; // Covariance matrix
+    glm::vec3 pos; // centre point
+    glm::vec3 col; // color
+    float alpha;
 };
 
 int foo();
 
 // This funcion will return gaussian color for given pixel index
 pixel getGaussianValue(uint32_t idx);
+
+void projectGaussian(const Gaussian& g, const glm::mat4 &proj);
--- CPU_gaussian_renderer/main.cpp
+++ CPU_gaussian_renderer/main.cpp
@@ -1,10 +1,12 @@
 #include "ppm.hpp"
+#include "gaussian.hpp"
 #include <iostream>
 
 int main()
 {
-    constexpr int width = 800;
-    constexpr int height = 600;
+    constexpr int width = 1920;
+    constexpr int height = 1080;
+
 
     pixel pixels[width * height] = {
         {255,   0,   0}, // red
@@ -27,6 +29,15 @@ int main()
         {255, 255,255},
         {255,255,255}
     };
+
+    glm::vec2 mean(0.0f, 0.0f);
+    glm::mat2 covariance(
+        0.10f,0.08f,
+        0.08f, 0.20f
+    );
+
+    glm::mat2 invCov = glm::inverse(covariance);
+
     std::ofstream file("image.ppm", std::ios::binary);
 
     file << "P6\n";
@@ -37,9 +48,27 @@ int main()
     {
         for(int w = 0; w < width; w++)
         {
+            // [-1.0,1.0]
+            float x = 2.0f * (w + 0.5f) / static_cast<float>(width - 1) - 1.0f;
+            float y = 2.0f * (h + 0.5f) / static_cast<float>(height - 1) - 1.0f;
+
+            glm::vec2 p(x,y);
+
+            glm::vec2 d = p - mean;
+
+            // d^T * Sigma^-1 * d
+            float q = glm::dot(d, invCov * d);
+
+            float density = std::exp(-0.5f * q);
+
+            int c_int = static_cast<int>(255.0f * density);
+            std::uint8_t c_uint = static_cast<std::uint8_t>(c_int);
+
+            pixel pix = {c_uint,c_uint,c_uint};
+
             file.write(
-                reinterpret_cast<const char*>(background),
-                sizeof(background)
+                reinterpret_cast<const char*>(&pix),
+                sizeof(pixel)
             );
         }
     }
