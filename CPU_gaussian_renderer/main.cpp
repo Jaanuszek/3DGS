55f6b4900e68ce93b2bff2baa4309114f3cb38fa
Jaanuszek <kubex564@gmail.com>
2026-09-04 20:23:26 +0200

    Trying to figurre out the basis view and perspective matrices from the scratch

--- CPU_gaussian_renderer/main.cpp
+++ CPU_gaussian_renderer/main.cpp
@@ -1,3 +1,4 @@
+#include "glm/ext/matrix_clip_space.hpp"
 #include "glm/fwd.hpp"
 #include "ppm.hpp"
 #include "gaussian.hpp"
@@ -8,12 +9,13 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include "glm/gtx/string_cast.hpp"
 
-
-
 int main()
 {
-    constexpr int width = 1920;
-    constexpr int height = 1080;
+    constexpr int WIDHT = 1920;
+    constexpr int HEIGHT = 1080;
+    constexpr float FOV = static_cast<float>(WIDHT) / static_cast<float>(HEIGHT);
+    constexpr float NEAR = 0.1f;
+    constexpr float FAR = 100.0f;
 
     My::vec<float,3> vec3 = {1.0f, 2.0f, 3.0f};
 
@@ -33,8 +35,7 @@ int main()
         }
     };
 
-
-    pixel pixels[width * height] = {
+    pixel pixels[WIDHT * HEIGHT] = {
         {255,   0,   0}, // red
         {  0, 255,   0}, // green
         {  0,   0, 255}, // blue
@@ -64,10 +65,8 @@ int main()
 
     glm::mat2 invCov = glm::inverse(covariance);
 
-
     // create covariance matrix from scale matrix and rotation matrix
     // In paper rotation matrix is taken from quaternion
-
     glm::vec3 euler(1.0f, 1.0f, 1.0f);
 
     // quaternion ha to be normalized (pure rotation quaternion has to have length of 1 ||q|| = 1)
@@ -86,19 +85,43 @@ int main()
 
     std::cout << glm::to_string(covariance_m) << std::endl;
 
+    glm::mat4 projectionMat = glm::perspective(
+        glm::radians(45.0f),
+        FOV,
+        NEAR,
+        FAR
+    );
+
+    // glm::perspecitve creates projection in NDC [-1,1], but we need it in pixel values
+    // X and Y diagonal of this matrix are related with focal x and focal y
+    // fx = P00 * W/2
+    // fy = P11 * H/2
+    float fx = projectionMat[0][0] * WIDHT * 0.5f;
+    float fy = projectionMat[1][1] * HEIGHT * 0.5f;
+
     std::ofstream file("image.ppm", std::ios::binary);
 
     file << "P6\n";
-    file << width << " " << height << '\n';
+    file << WIDHT << " " << HEIGHT << '\n';
     file << "255\n";
 
-    for(int h = 0; h < height; h++)
+    for(int h = 0; h < HEIGHT; h++)
     {
-        for(int w = 0; w < width; w++)
+        for(int w = 0; w < WIDHT; w++)
         {
             // [-1.0,1.0]
-            float x = 2.0f * (w + 0.5f) / static_cast<float>(width - 1) - 1.0f;
-            float y = 2.0f * (h + 0.5f) / static_cast<float>(height - 1) - 1.0f;
+            float x = 2.0f * (w + 0.5f) / static_cast<float>(WIDHT - 1) - 1.0f;
+            float y = 2.0f * (h + 0.5f) / static_cast<float>(HEIGHT - 1) - 1.0f;
+            float z = 0;
+
+            const float S = 1.0 / (z * z);
+            // glm::mat2x3 2 columns 3 rows
+            glm::mat2x3 J = glm::mat2x3(
+                fx / z, 0, -(fx * x) * S,
+                0, fy / z, -(fy * y) * S
+            );
+
+            // std::cout << glm::to_string(J) << std::endl;
 
             glm::vec2 p(x,y);
 
